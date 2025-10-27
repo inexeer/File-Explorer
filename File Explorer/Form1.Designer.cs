8ea06b0335d16051309d3cdf0381ff3b1baf0781
inexeer <135962360+inexeer@users.noreply.github.com>
2025-10-27 16:57:42 +0000

    Working dir logic

--- File Explorer/Form1.Designer.cs
+++ File Explorer/Form1.Designer.cs
@@ -29,47 +29,85 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.trvTree = new System.Windows.Forms.TreeView();
             this.lvwFiles = new System.Windows.Forms.ListView();
-            this.imgPastas = new System.Windows.Forms.ImageList(this.components);
+            this.imgImagens = new System.Windows.Forms.ImageList(this.components);
+            this.imgListView = new System.Windows.Forms.ImageList(this.components);
+            this.btnBack = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // trvTree
             // 
             this.trvTree.BackColor = System.Drawing.Color.LightGray;
-            this.trvTree.Location = new System.Drawing.Point(12, 12);
+            this.trvTree.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trvTree.ImageIndex = 2;
+            this.trvTree.ImageList = this.imgImagens;
+            this.trvTree.Location = new System.Drawing.Point(12, 49);
             this.trvTree.Name = "trvTree";
-            this.trvTree.Size = new System.Drawing.Size(236, 478);
+            this.trvTree.SelectedImageIndex = 0;
+            this.trvTree.Size = new System.Drawing.Size(236, 555);
             this.trvTree.TabIndex = 0;
+            this.trvTree.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.trvTree_NodeMouseDoubleClick);
             // 
             // lvwFiles
             // 
             this.lvwFiles.BackColor = System.Drawing.Color.LightGray;
+            this.lvwFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lvwFiles.HideSelection = false;
-            this.lvwFiles.Location = new System.Drawing.Point(254, 12);
+            this.lvwFiles.LargeImageList = this.imgListView;
+            this.lvwFiles.Location = new System.Drawing.Point(254, 49);
             this.lvwFiles.Name = "lvwFiles";
-            this.lvwFiles.Size = new System.Drawing.Size(621, 478);
+            this.lvwFiles.Size = new System.Drawing.Size(991, 555);
             this.lvwFiles.TabIndex = 1;
             this.lvwFiles.UseCompatibleStateImageBehavior = false;
+            this.lvwFiles.DoubleClick += new System.EventHandler(this.lvwFiles_DoubleClick);
             // 
-            // imgPastas
+            // imgImagens
             // 
-            this.imgPastas.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
-            this.imgPastas.ImageSize = new System.Drawing.Size(16, 16);
-            this.imgPastas.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgImagens.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgImagens.ImageStream")));
+            this.imgImagens.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgImagens.Images.SetKeyName(0, "folder.png");
+            this.imgImagens.Images.SetKeyName(1, "hard-drive.png");
+            this.imgImagens.Images.SetKeyName(2, "blank-page.png");
+            // 
+            // imgListView
+            // 
+            this.imgListView.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgListView.ImageStream")));
+            this.imgListView.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgListView.Images.SetKeyName(0, "folder.png");
+            this.imgListView.Images.SetKeyName(1, "hard-drive.png");
+            this.imgListView.Images.SetKeyName(2, "blank-page.png");
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnBack.Location = new System.Drawing.Point(12, 12);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(0);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(47, 31);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "←";
+            this.btnBack.UseVisualStyleBackColor = false;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.ClientSize = new System.Drawing.Size(887, 502);
+            this.ClientSize = new System.Drawing.Size(1257, 616);
+            this.Controls.Add(this.btnBack);
             this.Controls.Add(this.lvwFiles);
             this.Controls.Add(this.trvTree);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "FormMain";
             this.Text = "File Explorer";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Shown += new System.EventHandler(this.FormMain_Shown);
             this.ResumeLayout(false);
 
         }
@@ -78,7 +116,9 @@
 
         private System.Windows.Forms.TreeView trvTree;
         private System.Windows.Forms.ListView lvwFiles;
-        private System.Windows.Forms.ImageList imgPastas;
+        private System.Windows.Forms.ImageList imgImagens;
+        private System.Windows.Forms.ImageList imgListView;
+        private System.Windows.Forms.Button btnBack;
     }
 }
 
--- File Explorer/Form1.cs
+++ File Explorer/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace File_Explorer
 {
@@ -15,6 +17,95 @@ namespace File_Explorer
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            PopulateTreeView();
+        }
+
+        private void PopulateTreeView()
+        {
+            trvTree.Nodes.Clear();
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                TreeNode disko = new TreeNode(drive.Name);
+                disko.Tag = drive.Name;
+                disko.ImageIndex = 1;
+                disko.SelectedImageIndex = 1;
+                DirectoryInfo diskInfo = new DirectoryInfo(drive.Name);
+                foreach (var dir in diskInfo.GetDirectories())
+                {
+                    TreeNode directory = new TreeNode(dir.Name);
+                    directory.Tag = dir.FullName;
+                    directory.ImageIndex = 0;
+                    directory.SelectedImageIndex = 0;
+                    disko.Nodes.Add(directory);
+                }
+                foreach (var f in diskInfo.GetFiles())
+                {
+                    TreeNode file = new TreeNode(f.Name);
+                    file.ImageIndex = 2;
+                    file.SelectedImageIndex = 2;
+                    disko.Nodes.Add(file);
+                }
+                trvTree.Nodes.Add(disko);
+            }
+        }
+
+        private void PopulateListView(string path)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                DirectoryInfo[] subDirs = dir.GetDirectories();
+                FileInfo[] files = dir.GetFiles();
+                lvwFiles.Items.Clear();
+                foreach (var directory in subDirs)
+                {
+                    ListViewItem item = new ListViewItem(directory.Name);
+                    item.ImageIndex = 0;
+                    item.Tag = directory.FullName;
+                    lvwFiles.Items.Add(item);
+                }
+                foreach (var file in files)
+                {
+                    ListViewItem item = new ListViewItem(file.Name);
+                    item.ImageIndex = 2;
+                    item.Tag = file.FullName;
+                    lvwFiles.Items.Add(item);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show($"Access denied to folder:\n{path}", "Access Denied");
+            }
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            this.ActiveControl = null;
+        }
+
+        private void trvTree_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if(e.Node.Parent != null)
+            {
+                PopulateListView(e.Node.Tag.ToString());
+            }
+        }
+
+        private void lvwFiles_DoubleClick(object sender, EventArgs e)
+        {
+            string path = lvwFiles.SelectedItems[0].Tag.ToString();
+            if (lvwFiles.SelectedItems[0].ImageIndex == 0)
+            {
+                PopulateListView(path);
+            }
+            else
+            {
+                Process.Start(path);
+            }
         }
     }
 }
